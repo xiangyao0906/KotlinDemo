61846fd758bace34d35fd4bd53009ebe652114d9
xiangyao <sodagreen1994@163.com>
2017-11-03 09:30:29 +0800

    Initial commit

---
+++ src/com/yizhilu/xiangyao/KotlinDemo.kt
@@ -0,0 +1,20 @@
+package com.yizhilu.xiangyao
+
+
+fun main(args: Array<String>) {
+    haha()
+
+}
+
+
+fun haha() {
+    val mapOf = mapOf<String, String>("xiangyao" to "向垚")
+
+    print(mapOf["xiangyao"])
+
+    for (o in mapOf) {
+        print(o.key)
+    }
+
+
+}
